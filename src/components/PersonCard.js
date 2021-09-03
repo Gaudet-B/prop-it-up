082eb6c8c8cf2f7b19459ba744676845fbfd6572
Brian Gaudet <brian.f.gaudet@gmail.com>
2021-09-03 14:11:56 -0700

    added 'birthday' buttons to increment age

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react"
 
 class PersonCard extends Component {
-
+    constructor(props) {
+        super(props)
+        this.state = {
+            age: this.props.age,
+            count: 0
+        }
+    }
+    increaseAge = (e) => {
+        this.setState({count: this.state.count+1})
+        return this.setState({age: this.state.age+1})
+    }
     render() {
-        const {firstName, lastName, age, hairColor} = this.props;
+        const {firstName, lastName, hairColor} = this.props;
+        const {age} = this.state;
         return(
             <div>
                 <h2>{lastName}, {firstName}</h2>
                 <p>Age: {age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={ this.increaseAge }>Birthday Button for {firstName} {lastName}</button>
             </div>
         )
     }
